ec611aec5a746bb41f14ec70eb4ced70580b2632
Trifall <JerrenT@Trifall.com>
2023-12-03 20:21:03 -0500

    Initialize for day 1

---
+++ src/_util.ts
@@ -0,0 +1,14 @@
+export const GetFileData = async (filePath: string) => {
+	const inputFile = Bun.file(filePath);
+
+	if (!(await inputFile.exists())) {
+		console.error(`Couldn't retrieve file with path '${filePath}'`);
+		process.exit(0);
+	}
+
+	const fileData = await inputFile.text();
+	if (!fileData) throw Error(`Couldn't get text data from file.`);
+
+	console.log('Loaded Input File');
+	return fileData;
+};
