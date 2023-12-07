f1271df0f67e58a1f93d79e1d5ff3fb9fe990074
Trifall <JerrenT@Trifall.com>
2023-12-07 02:26:34 -0500

    setup day 8

---
+++ src/day8/day8.ts
@@ -0,0 +1,11 @@
+import { GetFileData } from '../util';
+
+const inputData = await GetFileData();
+
+const lines = inputData.split('\n');
+console.time(`Process Time`);
+
+console.log(`Part 1: n/a`);
+console.log(`Part 2: n/a`);
+
+console.timeEnd(`Process Time`);
